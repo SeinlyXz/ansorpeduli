878675ca55128a6a0b0c39dc92526e4b0515144f
SeinlyXz <macbookpro@GoodLuck.local>
2025-01-07 19:29:53 +0700

    feat: Added capture camera

--- src/routes/darurat/[category]/+page.svelte
+++ src/routes/darurat/[category]/+page.svelte
@@ -4,32 +4,48 @@
 	import Seo from '$lib/components/Seo.svelte';
 	import EasyCamera from '@cloudparker/easy-camera-svelte';
     import Camera from '$lib/logos/Camera.png';
+	import image1 from '$lib/images/Image1.jpg';
 
-	let width = $state(300);
+	let width = $state(350);
 	let camera: EasyCamera;
+	let cameraState = $state(false)
+	let images: string[] = $state([])
 
 	const handleImage = async () => {
 		let imageData = await camera.captureImage();
-	};
-
-	const handleVideo = async () => {
-		let blob = await camera.startVideoRecording();
-		if (blob) {
-			const url = URL.createObjectURL(blob);
-			const a = document.createElement('a');
-			a.href = url;
-			a.download = 'video.mp4';
-			document.body.appendChild(a);
-			a.click();
+		console.log(imageData)
+		if(images.length < 3){
+			images = [...images, `data:image/jpeg;base64,${imageData}`];
+			console.log(images)
+		} else {
+			return
 		}
 	};
 
+	// const handleVideo = async () => {
+	// 	let blob = await camera.startVideoRecording();
+	// 	if (blob) {
+	// 		const url = URL.createObjectURL(blob);
+	// 		const a = document.createElement('a');
+	// 		a.href = url;
+	// 		a.download = 'video.mp4';
+	// 		document.body.appendChild(a);
+	// 		a.click();
+	// 	}
+	// };
+
     const openCamera = () => {
         // @ts-ignore
-        document?.getElementById('my_modal_1')?.showModal();
+        document?.getElementById('open_camera_modal')?.showModal();
+		cameraState = true
+		camera.open()
     }
 
-	let mirrorDisplay = $state(true);
+	const closeCamera = () => {
+		cameraState = false
+	}
+
+	let mirrorDisplay = $state(false);
 	let pathname = page.url.pathname;
 </script>
 
@@ -96,13 +112,32 @@
 		</label>
 		<label for="file" class="flex flex-col gap-y-2">
 			<p class="font-bold">Upload Foto dari Kamera</p>
+			<section class="flex gap-x-3 justify-between">
+				{#if images.length == 0}
+					{#each Array(3) as _, i}
+						<div class="w-52 h-24 bg-gray-100 border border-gray-400 border-dashed rounded-xl relative flex justify-center items-center">
+							<p class="text-gray-500">
+								Kosong
+							</p>
+						</div>
+					{/each}
+				{:else}
+					{#each images as image, i}
+						<div class="w-52 h-24 bg-gray-100 border rounded-xl relative">
+							<button class="absolute -right-2 -top-2" type="button" aria-label="Remove image">
+								<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" class="bg-white rounded-full p-0">
+									<path d="M10 1.875C8.39303 1.875 6.82214 2.35152 5.486 3.24431C4.14985 4.1371 3.10844 5.40605 2.49348 6.8907C1.87852 8.37535 1.71762 10.009 2.03112 11.5851C2.34463 13.1612 3.11846 14.6089 4.25476 15.7452C5.39106 16.8815 6.8388 17.6554 8.4149 17.9689C9.99099 18.2824 11.6247 18.1215 13.1093 17.5065C14.594 16.8916 15.8629 15.8502 16.7557 14.514C17.6485 13.1779 18.125 11.607 18.125 10C18.1227 7.84581 17.266 5.78051 15.7427 4.25727C14.2195 2.73403 12.1542 1.87727 10 1.875ZM12.9422 12.0578C13.0003 12.1159 13.0463 12.1848 13.0777 12.2607C13.1092 12.3366 13.1254 12.4179 13.1254 12.5C13.1254 12.5821 13.1092 12.6634 13.0777 12.7393C13.0463 12.8152 13.0003 12.8841 12.9422 12.9422C12.8841 13.0003 12.8152 13.0463 12.7393 13.0777C12.6634 13.1092 12.5821 13.1253 12.5 13.1253C12.4179 13.1253 12.3366 13.1092 12.2607 13.0777C12.1848 13.0463 12.1159 13.0003 12.0578 12.9422L10 10.8836L7.94219 12.9422C7.88412 13.0003 7.81518 13.0463 7.73931 13.0777C7.66344 13.1092 7.58213 13.1253 7.5 13.1253C7.41788 13.1253 7.33656 13.1092 7.26069 13.0777C7.18482 13.0463 7.11588 13.0003 7.05782 12.9422C6.99975 12.8841 6.95368 12.8152 6.92226 12.7393C6.89083 12.6634 6.87466 12.5821 6.87466 12.5C6.87466 12.4179 6.89083 12.3366 6.92226 12.2607C6.95368 12.1848 6.99975 12.1159 7.05782 12.0578L9.11641 10L7.05782 7.94219C6.94054 7.82491 6.87466 7.66585 6.87466 7.5C6.87466 7.33415 6.94054 7.17509 7.05782 7.05781C7.17509 6.94054 7.33415 6.87465 7.5 6.87465C7.66586 6.87465 7.82492 6.94054 7.94219 7.05781L10 9.11641L12.0578 7.05781C12.1159 6.99974 12.1848 6.95368 12.2607 6.92225C12.3366 6.89083 12.4179 6.87465 12.5 6.87465C12.5821 6.87465 12.6634 6.89083 12.7393 6.92225C12.8152 6.95368 12.8841 6.99974 12.9422 7.05781C13.0003 7.11588 13.0463 7.18482 13.0777 7.26069C13.1092 7.33656 13.1254 7.41788 13.1254 7.5C13.1254 7.58212 13.1092 7.66344 13.0777 7.73931C13.0463 7.81518 13.0003 7.88412 12.9422 7.94219L10.8836 10L12.9422 12.0578Z" fill="#E22020"/>
+								</svg>
+							</button>
+							<img src="{image}" alt="" class="w-full h-full object-cover rounded-xl">
+						</div>
+					{/each}
+				{/if}
+			</section>
             <button class="flex justify-center py-4 gap-x-3 bg-[#F6F7F7] text-[#9FA9A2] rounded-[8px] border border-dashed border-[#E5E7EB]" onclick={openCamera}>
                 <img src="{Camera}" alt="" class="w-[26px] h-[23px]">
                 Klik untuk mengambil gambar
             </button>
-			<!-- <button class="" type="button" onclick={handleImage}>Capture Image</button> -->
-			<!-- <button class="" type="button" onclick={() => camera.open()}>Start Camera</button> -->
-			<!-- <button class="" type="button" onclick={() => camera.close()}>Stop Camera</button> -->
 		</label>
 		<label for="confirm" class="flex items-center gap-x-3">
 			<input
@@ -117,21 +152,26 @@
 			>LAPOR SEKARANG</button
 		>
 	</form>
-	<dialog id="my_modal_1" class="modal">
+	<dialog id="open_camera_modal" class="modal">
 		<div class="modal-box">
-			<section>
+			<section class="pt-5">
                 <EasyCamera
                     bind:width
                     style="border-radius:5px;"
                     bind:this={camera}
-                    autoOpen
+					autoOpen={cameraState}
                     bind:mirrorDisplay
                 />
+				<section class="flex justify-center pt-5">
+					<button class="btn btn-lg btn-circle bg-green-500 text-white" type="button" onclick={handleImage} aria-label="Take a picture">
+						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-camera"><path d="M14.5 4h-5L7 7H4a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2h-3l-2.5-3z"/><circle cx="12" cy="13" r="3"/>
+						</svg>
+					</button>
+				</section>
             </section>
 			<div class="modal-action">
-				<form method="dialog">
-					<!-- if there is a button in form, it will close the modal -->
-					<button class="btn">Close</button>
+				<form method="dialog" class="flex">
+					<button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2" onclick={closeCamera}>✕</button>
 				</form>
 			</div>
 		</div>
